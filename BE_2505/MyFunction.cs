e2ac52027fbfaf02e85d0a84f62be01bc0596b46
quan91 <quan91.nt@gmail.com>
2024-05-25 21:57:56 +0700

    bài 1

---
+++ BE_2505/MyFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_2505
+{
+    public static class MyFunction
+    {
+        public static int CongHaiSo(int soThuNhat, int soThuHai)
+        {
+            return soThuNhat + soThuHai;
+        }
+    }
+
+}
--- BE_2505/Program.cs
+++ BE_2505/Program.cs
@@ -1,1 +1,19 @@
 ﻿Console.WriteLine("XIN CHAO CÁC BẠN");
+int myValiable = 10;
+string name = "my name";
+
+var myName = "Quan";
+var myAge = 20;
+
+myValiable = 15;
+
+const string myValue = "abc";
+
+int x = 5;
+int y = --x; //4 
+Console.WriteLine("--y:={0}", y);
+
+int x1 = 5;
+int y1 = x1--;// 5 ?? 4
+Console.WriteLine("y1---:={0}", y1);
+Console.WriteLine(myValiable);
